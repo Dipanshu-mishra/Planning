c4489308d7685ff1884fb1cb1ed19a02ac176ab3
Dipanshu-mishra <mdipanshu464@gmail.com>
2023-12-22 00:59:01 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = [
+    {
+        id: 1,
+        name: "Varanasi",
+        info: " Varanasi is a city on the Ganges river in northern India that has a central place in the traditions of pilgrimage, death, and mourning in the Hindu world.[17][b] The city has a syncretic tradition of Muslim artisanship that underpins its religious tourism.[20] Located in the middle-Ganges valley in the southeastern part of the state of Uttar Pradesh, Varanasi lies on the left bank of the river. It is 692 kilometres (430 mi) to the southeast of India's capital New Delhi and 320 kilometres (200 mi) to the southeast of the state capital, Lucknow. It lies 121 kilometres (75 mi) downstream of Prayagraj, where the confluence with the Yamuna river is another major Hindu pilgrimage site.",
+        image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
+        price: "35,789",
+    },
+    {
+        id: 2,
+        name: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres (140 mi) south-east of the national capital Delhi and 330 km west of the state capital Lucknow. With a population of roughly 1.6 million, Agra is the fourth-most populous city in Uttar Pradesh and twenty-third most populous city in India.",
+        image: "https://cdn.thecodehelp.in/Agra.jpeg",
+        price: "44,980",
+    },
+    {
+        id: 3,
+        name: "Jaipur",
+        info: " Jaipur formerly Jeypore, is the capital and the largest city of the Indian state of Rajasthan. As of 2011, the city has a population of 3.1 million, making it the tenth most populous city in the country. Jaipur is also known as the Pink City due to the dominant colour scheme of its buildings and as the Paris of India. C. V. Raman called it the Island of Glory. It is located 268 km (167 miles) from the national capital New Delhi. Jaipur was founded in 1727 by the Kachhwaha Rajput ruler Jai Singh II,[12][13] the ruler of Amer, after whom the city is named. It is one of the earliest planned cities of modern India, designed by Vidyadhar Bhattacharya.[14] During the British Colonial period, the city served as the capital of Jaipur State. After independence in 1947, Jaipur was made the capital of the newly formed state of Rajasthan.",
+        image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price: "54,326",
+    },
+    {
+        id: 4,
+        name: "Darjeeling",
+        info: "Darjeeling is a town and municipality in the northernmost region of the Indian state of West Bengal. Located in the Eastern Himalayas, it has an average elevation of 2,045 metres (6,709 ft).[10] To the west of Darjeeling lies the easternmost province of Nepal, to the east the Kingdom of Bhutan, to the north the Indian state of Sikkim, and farther north the Tibet Autonomous Region region of China. Bangladesh lies to the south and southeast, and most of the state of West Bengal lies to the south and southwest, connected to the Darjeeling region by a narrow tract. Kangchenjunga, the world's third-highest mountain, rises to the north and is prominently visible on clear days.",
+        image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+        price: "76,867",
+    },
+    {
+        id: 5,
+        name: "Kochi",
+        info: "Kochi, also known by its former name Cochin is a major port city along the Malabar Coast of India bordering the Laccadive Sea. It is part of the district of Ernakulam in the state of Kerala. The city is also commonly referred to as Ernakulam. As of 2011, the Kochi Municipal Corporation had a population of 677,381[4] within an area of 94.88 km2, and the larger Kochi urban agglomeration had a total population of more than of 2.1 million within an area of 440 km2, making it the largest and the most populous metropolitan area in Kerala. Kochi city is also part of the Greater Cochin development region[10] and is classified as a Tier-II city by the Government of India. The civic body that governs the city is the Kochi Municipal Corporation, which was constituted in the year 1967, and the statutory bodies that oversee its development are the Greater Cochin Development Authority[11] (GCDA) and the Goshree Islands Development Authority .",
+        image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+        price: "83,550",
+    },
+    {
+        id: 6,
+        name: "Jaisalmer",
+        info: "Jaisalmer, nicknamed The Golden city , is a city in the Indian state of Rajasthan, located 575 kilometres (357 mi) west of the state capital Jaipur. It is the administrative headquarters of Jaisalmer District. Before Indian independence, the town served as the capital of the Jaisalmer State, ruled by the Bhati Rajputs.[7] Jaisalmer stands on a ridge of yellowish sandstone and is crowned by the ancient Jaisalmer Fort. This fort contains a royal palace and several ornate Jain temples. Many of the houses and temples of both the fort and of the town below are built of finely sculptured sandstone. The town lies in the heart of the Thar Desert (the Great Indian Desert) and has a population, including the residents of the fort, of about 78,000.",
+        image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+        price: "59,534",
+    },
+    {
+        id: 7,
+        name: "Goa",
+        info: "Goa is a state on the southwestern coast of India within the Konkan region, geographically separated from the Deccan highlands by the Western Ghats.[6][7] It is bound by the Indian states of Maharashtra to the north, and Karnataka to the east and south, with the Arabian Sea in the west. It is India's smallest state by area and fourth-smallest by population. Goa has the highest GDP per capita among all Indian states, two and a half times as high as the GDP per capita of the country as a whole. The Eleventh Finance Commission of India named Goa the best-placed state because of its infrastructure, and India's National Commission on Population rated it as having the best quality of life in India (based on the commission's. It is the second-highest ranking among Indian states in the human development index.",
+        image: "https://cdn.thecodehelp.in/Goa.jpeg",
+        price: "98,344",
+    },
+]
+
+export default data;
